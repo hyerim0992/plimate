d25d81c460dabfe4cec66c73095eff3716420939
hyerim <20200992@sungshin.ac.kr>
2023-06-05 15:26:00 +0000

    최신 커밋

--- main.js
+++ main.js
@@ -86,13 +86,12 @@ app.get('/',async(req,res)=>{
 
 app.post('/toggleplay', async (req, res) => {
   const devices = await spotifyApi.getMyDevices();
-  console.log(devices.body.devices[0][0]); //여기서 찍어보고 해당된 아이디 가져오고 아래 코드에 경로 추가하기 
-    const id = devices.body.devices.id //추가하기
+    const id = devices.body.devices[0].id //추가하기
 	console.log(id)
 	await spotifyApi.transferMyPlayback([id],true)
     await spotifyApi.play({ device_id: id, uris: ['spotify:track:7tajvm3L4vnNsOyMBf3yq3'] });
+	console.log(devices.body); //여기서 찍어보고 해당된 아이디 가져오고 아래 코드에 경로 추가하기 
     console.log('/toggleplay');
-	res.redirect('/');
 });
 
 app.get('/callback', function(req, res) {
